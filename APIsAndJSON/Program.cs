5484231a0f596c40bd0b4e4c18e20b5a7a350d78
Theo Boateng <Theoboateng2002@gmail.com>
2026-04-06 00:11:05 -0400

    completed exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+class Program
 {
-    public class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        var client = new HttpClient();
+
+        for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine("Hello, World");
+            // Ron Swanson API
+            var ronResponse = await client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
+            var ronQuote = JArray.Parse(ronResponse).ToString()
+                .Replace("[", "")
+                .Replace("]", "")
+                .Replace("\"", "")
+                .Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}");
+
+            // Kanye API
+            var kanyeResponse = await client.GetStringAsync("https://api.kanye.rest");
+            var kanyeJson = JObject.Parse(kanyeResponse);
+            var kanyeQuote = kanyeJson["quote"].ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+            Console.WriteLine();
         }
     }
 }
